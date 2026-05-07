ec07e243bde756b6acba470595c1aa9d73f81929
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-05-06 22:07:36 -0600

    movimiento de casillas en windos

--- nave.cpp
+++ nave.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
 int main(){
-
-
+    std::cout << "*" << std::endl;
+    HANDLE hconsole;
+    hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hconsole, (COORD){10, 5});
+    std::cout << "*" << std::endl;
     return 0;
 }
