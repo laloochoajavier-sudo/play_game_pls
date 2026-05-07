5786a2156335936b72ba7457c0f56affb1204a15
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-05-07 15:05:41 -0600

    como mover un asterisco con el wdsa

--- nave.cpp
+++ nave.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
+void gotoxy(int x, int y){
+    HANDLE hconsole;
+    hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(hconsole, coord);
+}
 int main(){
-HANDLE hconsole;
-hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
-std::cout << "*" << std::endl;
-    SetConsoleCursorPosition(hconsole, (COORD){10, 5}); //la posicion del cursor en la consola
-        std::cout << "*" << std::endl;// Y esta invertida para que se imprima en la posicion del cursor
-    SetConsoleCursorPosition(hconsole, (COORD){5, 10});
-        std::cout << "*" << std::endl;
+    int x = 10, y = 10;
+    gotoxy(x, y); std::cout << "*" << std::endl;
+    bool gameover = false;
+    while(!gameover){
+    char tecla = _getch();
+    gotoxy(x,y); std::cout<<" "<<'\n';
+        switch(tecla){
+            case 'w': y--; break;
+            case 's': y++; break;
+            case 'a': x--; break;
+            case 'd': x++; break;
+        }
+        gotoxy(x, y);std::cout << "*" << std::endl;
+        Sleep(30);    
+    }
     return 0;
 }
