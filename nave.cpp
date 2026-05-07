d30a834f02b6a3393ac7b40fb8fae2c772b94682
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-05-06 22:12:56 -0600

    windows.h es muy util para cosas curiosas

--- nave.cpp
+++ nave.cpp
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <windows.h>
 int main(){
-    std::cout << "*" << std::endl;
-    HANDLE hconsole;
-    hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(hconsole, (COORD){10, 5});
-    std::cout << "*" << std::endl;
+HANDLE hconsole;
+hconsole = GetStdHandle(STD_OUTPUT_HANDLE);
+std::cout << "*" << std::endl;
+    SetConsoleCursorPosition(hconsole, (COORD){10, 5}); //la posicion del cursor en la consola
+        std::cout << "*" << std::endl;// Y esta invertida para que se imprima en la posicion del cursor
+    SetConsoleCursorPosition(hconsole, (COORD){5, 10});
+        std::cout << "*" << std::endl;
     return 0;
 }
